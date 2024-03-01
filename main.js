863d01230053eb2e9f001af7c7ee45ca18866e1f
shahid <6shahid6@gmail.com>
2024-03-01 11:59:52 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+"use strict";
+//store a persons name in a variable and print a message to that person your message should be simple such as hello shahid would like to learn some python today//
+let personname = "shahid";
+let message = `hey ${personname} would you like to learn typescript today`;
+console.log(message);
---
+++ main.ts
@@ -0,0 +1,4 @@
+//store a persons name in a variable and print a message to that person your message should be simple such as hello shahid would like to learn some python today//
+let personname: string = "shahid"
+let message: string = `hey ${personname} would you like to learn typescript today`
+console.log(message)
